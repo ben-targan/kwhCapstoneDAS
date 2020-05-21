f0aa72d5c9281e7c9a72bc91641ccd9c3b16049a
Ben Targan <ben.targan@gmail.com>
2020-05-20 17:51:55 -0700

    For testing insert quickly
    KWH_MySQL.py was not present on testbench, so in order to test SQL functionality must temporarily copy the method into this file

--- vedirect.py
+++ vedirect.py
@@ -1,13 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Charge Controller Processing
+import sys
 import os, serial, argparse
+import serial.tools.list_ports as listPorts
+import subprocess
+import time
+
+# KWH debug, spans system
+DEBUG = int(config_var['DEBUG'])
+
+# Log function
+def log(logText):
+    with open("/kwh/log/modbus.log", "a+") as log:
+        log.write(str(int(time.time())) + ": " + logText +"\n")
+
+
+# debug flag for additional printing
+debug = False
+
 
 class vedirect:
     
-    def __init__(self, serialport, timeout):
+    def __init__(self, serialport, timestamp):
+        self.timestamp = timestamp
         self.serialport = serialport
-        self.ser = serial.Serial(serialport, 19200, timeout=timeout)
+        self.ser = serial.Serial(serialport, 19200)
         self.carrigeReturn = '\r'
         self.newLine = '\n'
         self.colon = ':'
@@ -27,15 +46,18 @@ class vedirect:
         if byte == self.colon and self.currState != self.IN_CHECKSUM:
             self.currState = self.HEX
             
-#---------------------------------------------------------------------
+        #---------------------------------------------------------------------
         
         if self.currState == self.WAIT_HEADER:
-            print("### In Wait, ord: ", ord(byte))
+
+            if debug:
+                print("### In Wait, ord: ", ord(byte))
             try:
                 self.packetLen += ord(byte) #ord throws: given char arr len 0
 
             except TypeError:
-                print("Malformed packet --wait") #inverter hangs here
+                if debug:
+                    print("Malformed packet --wait") #inverter hangs here
 
             if byte == self.carrigeReturn:
                 self.currState = self.WAIT_HEADER
@@ -45,15 +67,17 @@ class vedirect:
 
             return None
 
-#---------------------------------------------------------------------
+        #---------------------------------------------------------------------
 
         elif self.currState == self.IN_KEY:
-            print("### In Key")
+            if debug:
+                print("### In Key")
             try:
                 self.packetLen += ord(byte)
 
             except TypeError:
-                print("Malformed packet --inkey")
+                if debug:
+                    print("Malformed packet --inkey")
             
             if byte == self.tab:
                 if (self.key == 'Checksum'):
@@ -67,15 +91,17 @@ class vedirect:
             
             return None
         
-#---------------------------------------------------------------------        
+        #---------------------------------------------------------------------        
 
         elif self.currState == self.IN_VALUE:
-            print("### In Value")
+            if debug:
+                print("### In Value")
             try:
                 self.packetLen += ord(byte)
 
             except TypeError: 
-                print("Malformed packet --invalue")
+                if debug:
+                    print("Malformed packet --invalue")
             
             if byte == self.carrigeReturn:
                 self.currState = self.WAIT_HEADER
@@ -89,14 +115,16 @@ class vedirect:
             
             return None
 
-#---------------------------------------------------------------------
+        #---------------------------------------------------------------------
 
         elif self.currState == self.IN_CHECKSUM:
-            print("### In Checksum")
+            if debug:
+                print("### In Checksum")
             try:
                 self.packetLen += ord(byte)
             except TypeError:
-                print("Malformed packet --checksum")
+                if debug:
+                    print("Malformed packet --checksum")
             
             self.key = ''
             self.value = ''
@@ -107,65 +135,149 @@ class vedirect:
                 return self.packetDict #VALID PACKET RETURN
 
             else:
-                print("Malformed packet")
+                if debug:
+                    print("Malformed packet --incorrect packetLen")
                 self.packetLen = 0
 
-#---------------------------------------------------------------------                
+        #---------------------------------------------------------------------                
 
         elif self.currState == self.HEX:
-            print("### In Hex")
+            if debug:
+                print("### In Hex")
             self.packetLen = 0
 
             if byte == self.newLine:
                 self.currState = self.WAIT_HEADER
 
-#---------------------------------------------------------------------
+        #---------------------------------------------------------------------
 
         else:
-            print("### In assertionError")
+            if debug:
+                print("### In assertionError")
             raise AssertionError()
-
-    # unused methods:
-    # def read_data(self):
-    #     while True:
-    #         byte = self.ser.read(1)
-    #         packet = self.input(byte).encode('utf-8').strip()
-
-    # def read_data_single(self):
-    #     while True:
-    #         byte = self.ser.read(1)
-    #         packet = self.input(byte).encode('utf-8').strip()
-    #         if (packet != None):
-    #             return packet
             
 
-    def read_data_callback(self, callbackFunction):
+#-----------------------------------------------------------------------------
+    def read(self, sendingFunction):
         while True:
             byte = self.ser.read(1)
             if byte:
                 try:
-                    packet = self.input(byte.decode())
+                    packet = self.input(byte.decode('windows-1252', errors="ignore"))
                 except UnicodeError:
-                    #print("NON UTF CHAR")
-                    packet = self.input(byte.decode('windows-1252')) #Guess another encoding, doesnt error, but inverter returns Euro sign & '/x00'
+                    if debug:
+                        print("NON win1252 CHAR")
+                    packet = self.input(byte.decode('utf-8', errors="ignore"))
+                    # packet = self.input(byte.decode('windows-1252')) #Guess another encoding, doesnt error, but inverter returns Euro sign & '/x00'
                 else:
                     pass
                 if (packet != None):
-                    callbackFunction(packet) #packet is complete at this point. type=dict
+                    sendingFunction(packet, self.timestamp) #packet is complete at this point. type=dict
             else:
                 print("No byte, break occured.")
                 break
 
 
-##### EXTRACT OUTPUT DICT FROM THIS METHOD
-def print_data_callback(data):
-    print(data)
+#-----------------------------------------------------------------------------
+def convertKeys(data):
+    keysDict = {
+        "PPV" : "PV Array Power",
+        "VPV" : "PV Array Voltage",
+        "LOAD" : "Load",
+        "H19" : "h19",
+        "Relay" : "Relay",
+        "ERR" : "Error #",
+        "FW" : "FW",
+        "I" : "Main Current",
+        "H21" : "h21",
+        "PID" : "Process ID",
+        "H20" : "h20",
+        "H23" : "h23",
+        "MPPT" : "Maximum Power Point",
+        "HSDS" : "HSDS",
+        "SER#" : "Serial #",
+        "V" : "Main Voltage",
+        "CS" : "CS",
+        "H22" : "h22"
+    }
+    newdata = {}
+
+    for key in data:
+
+        try:
+            newdata[keysDict[key]] = data[key]
+        except KeyError:
+            newdata[key] = data[key]
+
+    return newdata
+
+#-----------------------------------------------------------------------------
+
+
+
+def INSERT(sql):
+    import MySQLdb
+    from MySQLdb import Error
+
+    try:
+        db = MySQLdb.connect('localhost','pi','','kwh')
+        cursor = db.cursor()
+        result = cursor.execute(sql)
+        db.commit()
+        cursor.close()
+        db.close()
+        
+    except MySQLdb.Error as error:
+        db.rollback()
+        cursor.close()
+        db.close()
+        return [1, error]
+
+    return [0]
+
+
+
+def sendToSQL(data, timestamp):
+
+    data = convertKeys(data)
+
+    # insert in format timestamp, label, value
+    DB = KWH_MySQL.KWH_MySQL()
+
+    for key in data:
+        sql="INSERT INTO data VALUES (" + timestamp +","+ key.encode("utf-8") + "," + data[key].encode("utf-8") + ");"
+        INSERT(sql)
+        if DEBUG: log(sql)
+
+
+def printToConsole(data, timestamp):
+
+    data = convertKeys(data)
+
+    print("-----------------------------------------------------")
+    for key in data:
+        print("(%s)%s : %s" % (timestamp, key.encode("utf-8"), data[key].encode("utf-8")))
+    print("-----------------------------------------------------")
+
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process VE.Direct protocol')
-    parser.add_argument('--port', help='Serial port')
-    parser.add_argument('--timeout', help='Serial port read timeout', type=int, default='60')
-    args = parser.parse_args()
-    ve = vedirect(args.port, args.timeout)
-    ve.read_data_callback(print_data_callback)
-    #print(ve.read_data_single())
+    correctPort = ''
+    timestamp = sys.argv[1]
+
+    possiblePorts = listPorts.comports()
+
+    for port in possiblePorts:
+        if port.description == 'VE Direct cable':
+            correctPort = port.device
+
+
+    #TODO: add logging
+    if correctPort == '':
+        log("Serial Port for Charge Controller not found, exiting...")
+        sys.exit(0)
+
+
+    ve = vedirect(correctPort, timestamp)
+    # ve.read(sendToSQL)
+    ve.read(printToConsole)
